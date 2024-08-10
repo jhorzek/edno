12c760cf8acd29c0c84185f7a5fec1776e4be3b9
Jannik H. Orzek <jannik.orzek@mailbox.org>
2024-08-11 00:02:20 +0200

    Changing bg color

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 import edno
 
 if __name__ == "__main__":
-    ctk.set_appearance_mode("Light")
+    ctk.set_appearance_mode("Dark")
     ctk.set_default_color_theme(
         "blue"
     )
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     root.geometry("1000x850")
     root.grid_columnconfigure(0, weight=1)
     root.grid_rowconfigure(0, weight=1)
-    dag = edno.EdnoCanvas(root)
+    dag = edno.EdnoCanvas(root, node_color="lightblue")
     dag.grid(row=0, column=0, sticky="nsew")
     root.mainloop()
